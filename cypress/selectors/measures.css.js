2c4d9b72154c5c411757cd0d92a3ee483c9658f6
Gabe Zoldi <gabez@westmarine.com>
2024-10-29 14:09:23 -0700

    fixed some issues

---
+++ cypress/selectors/measures.css.js
@@ -0,0 +1,5 @@
+module.exports = {
+    riskAssessmentRadio: '#i5 .vd3tt div',
+    auditedRadio: '#i15 .vd3tt div div',
+    submitButton: 'Submit'
+}
--- cypress/selectors/risk.css.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    sensitiveDataRadio: '#i5 > div.vd3tt > div > div',
-    riskLevelRadio: '[name=email]',
-    backButton: 'Back',
-    nextButton: 'Next'
-}
